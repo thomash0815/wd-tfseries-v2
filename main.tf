05ae008a2412e96528a0df00d66dc6af3104e24b
Thomas Hafermalz <thomas.hafermalz@accenture.com>
2023-08-09 21:53:25 +0200

    remote module

--- main.tf
+++ main.tf
@@ -15,17 +15,32 @@ provider "azurerm" {
   features {}
 }
 
+
+resource "random_integer" "this" {
+  min = 1
+  max = 50000
+}
+
 resource "azurerm_resource_group" "web" {
   name     = var.rg_name
   location = var.location
   tags     = var.tags
 }
 
-module "app" {
-  source              = "./modules/webapp"
-  app_name            = var.app_name
+module "webapp" {
+  source = "git::https://github.com/thomash0815/tf-modules.git//webapp?ref=5387396" #v.1.0.0" #feature/demochange" #"./modules/webapp"
+
+  for_each            = var.webapp_names_list
+  webapp_name         = each.value
   resource_group_name = azurerm_resource_group.web.name
   location            = azurerm_resource_group.web.location
+  appserviceplan_name = "sp-${each.value}"
   sku_name            = var.sku_name
+  os_type             = var.os_type
+  identity_type       = var.identity_type
   tags                = var.tags
+}
+
+output "webapp_id" {
+  value = values(module.webapp)[*].webapp_id
 }
--- variables.tf
+++ variables.tf
@@ -1,29 +1,47 @@
-variable "app_name" {
-  description = "The name which should be used for this Linux Web App"
-  type        = string
+variable "resource_group_name" {
+  description = "Resource Group where Resources are deployed"
 }
-
-variable "sku_name" {
-  description = "Service Plan SKU"
-  type        = string
-  default     = "S1"
-}
-
-variable "rg_name" {
-  description = "Resource Group where the App Service Plan exists."
-  type        = string
-}
-
 variable "location" {
-  description = "The Region where the Web App should exist."
+  description = "Location where Product Resources are deployed"
+  default     = "north europe"
+}
+
+variable "appserviceplan_name" {
+  description = "Name of the App Service Plan"
   type        = string
-  default     = "North Europe"
+}
+variable "sku_name" {
+  description = "SKU of the App Service Plan"
+  type        = string
+}
+variable "os_type" {
+  description = "OS type of the App Service Plan"
+  type        = string
+  default     = "Linux"
+}
+
+variable "webapp_name" {
+  description = "Name of the web app"
+  type        = string
+}
+
+variable "webapp_names_list" {
+  description = "List of names for multiple web apps"
+  type        = set(string)
+}
+
+variable "identity_type" {
+  description = "Identity type for the Web App"
+  type        = string
+  default     = "SystemAssigned"
 }
 
 variable "tags" {
-  description = "Tags to set"
+  description = "Tags to set for all resources"
   type        = map(string)
+
   default = {
+    Costcenter  = "1234",
+    Environment = "dev"
   }
 }
-
